6f5860688f0ca0d2e256077a5dd0a62d5216214c
lcroley <logan.croley.18@gmail.com>
2024-05-22 23:26:13 -0400

    chore: updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
-import { useState, useEffect } from "react";
-import Container from "./components/Layout/Container";
-import Toggle from "./components/Toggle";
 import socket from "./utils/sockets/socket";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Game from "./pages/Game";
+import Enter from "./pages/Enter";
 
+export type User = {
+    name: string,
+    id: string
+} | null;
 
 function App() {
+    const [currentUser, setCurrentUser] = useState<User>(null);
+    const nav = useNavigate();
+
+    useEffect(()=>{
+        if(currentUser != null) {
+            nav("/game")
+        }
+    }, [currentUser])
+
+    useEffect(()=>{
+        if(currentUser == null) {
+            nav("/")
+        }
+    }, [])
+
+    const routes = <Routes>
+        <Route
+            path="/"
+            element={
+                <Enter currentUser={currentUser} setCurrentUser={setCurrentUser} />
+            }
+        />
+        <Route
+            path="/game"
+            element={
+                <Game socket={socket} />
+            }
+        />
+    </Routes>
+
     return (
-        <Container>
-            <Toggle socket={socket} id={"1"}/>
-            <Toggle socket={socket} id={"2"}/>
-        </Container>
-    );
+        <div>
+            {routes}
+        </div>
+    )
 }
 
-export default App
+export default App;
