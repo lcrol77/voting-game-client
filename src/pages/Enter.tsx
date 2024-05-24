f2be384a59142830fdae9de736df18c8d6daa992
lcroley <logan.croley.18@gmail.com>
2024-05-23 23:02:45 -0400

    feat: added ui elements to enter page

--- src/pages/Enter.tsx
+++ src/pages/Enter.tsx
@@ -8,7 +8,16 @@ export default function Enter(props: EnterProps) {
     };
 
     return (
-        <div><button onClick={() => props.setCurrentUser(userToBeSet)} className="bg-blue-500 text-white px-4 py-2 rounded border-2 border-blue-700 shadow-md">Set State</button></div>
+        <div className="bg-east-bay-400 space-y-4 flex flex-col items-center justify-center h-screen">
+            <h1 className="">Voting Game</h1>
+            <div className="mx-auto w-96 space-y-4 rounded-md border-2 border-east-bay-600 bg-white p-4 shadow-lg flex flex-col">
+                <div className="mt-2">
+                    <input className="placeholder: block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-east-bay-600 sm:text-sm sm:leading-6"
+                        placeholder="Your name here" type="text" />
+                </div>
+                <button className="bg-east-bay-400 rounded border-2 border-east-bay-600">Enter</button>
+            </div>
+        </div>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-	"./src/**/*.{js,jsx,ts,tsx}"
-  ],
-	content: [
-		"./src/**/*.{js,jsx,ts,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'east-bay': {
+                    '50': '#f4f7fa',
+                    '100': '#e6ecf3',
+                    '200': '#d3dcea',
+                    '300': '#b5c6db',
+                    '400': '#91a8c9',
+                    '500': '#778eba',
+                    '600': '#6579ab',
+                    '700': '#59699c',
+                    '800': '#4d5780',
+                    '900': '#434c6b',
+                    '950': '#2b2f40',
+                },
+
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
 
