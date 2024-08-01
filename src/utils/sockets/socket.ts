abc7c3b4aafad87905997b013b5c91f633f80f1a
lcroley <logan.croley.18@gmail.com>
2024-08-01 12:27:44 -0400

    feat: added support for env.local file to prevent issues with ips

--- src/utils/sockets/socket.ts
+++ src/utils/sockets/socket.ts
@@ -1,7 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-//const URL = "http://192.168.0.186:8080";
-const URL = "http://192.168.0.159:8080";
+const URL = `http://${process.env.REACT_APP_WSS_IP}:8080`;
 const socket: Socket = io(URL);
 
 export default socket;
